8d623835e6de5103f5dd8ea6fbf3aa643f584572
MatthewPlemmons <matthewleeplemmons@gmail.com>
2016-11-23 01:26:43 +0000

    edited header file

--- holberton.h
+++ holberton.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+typedef struct Cspecs {
+	char cs;
+	void (*f)(va_list);
+} cs_t;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
